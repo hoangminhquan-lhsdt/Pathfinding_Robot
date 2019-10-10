b5cc435a84f44c04598589e641de861cf0fa5b29
hac <laohacsieudeptrai@gmail.com>
2019-10-10 15:46:49 +0700

    edit Map class, add State class

--- Map.py
+++ Map.py
@@ -1,13 +1,13 @@
 class Map:
 	def __init__(self, row, col, start=(19,0), goal=(0,29)):
 		# create 2d array
-		self.map = [[0 for x in range(row)] for y in range(col)]
+		self._map = [[0 for x in range(row)] for y in range(col)]
 
 		# set start and goal
 		self.start = start
 		self.goal = goal
-		self.map[self.start[0]][self.start[1]] = 2
-		self.map[self.goal[0]][self.goal[1]] = 3
+		self._map[self.start[0]][self.start[1]] = 2
+		self._map[self.goal[0]][self.goal[1]] = 3
 
 		self.obstacleList = list()
 		self.pickupPoints = list()
@@ -15,13 +15,13 @@ class Map:
 	def addObstacle(self, pointsList):
 		for i in pointsList:
 			self.obstacleList.append(i)
-			self.map[i[0]][i[1]] = 1
+			self._map[i[0]][i[1]] = 1
 
 	def addPickup(self, pointsList):
 		pass
 
 	def printMap(self):
-		for i in self.map:
+		for i in self._map:
 			print(i)
 
 	def isBlocked(self, x, y):
--- main.py
+++ main.py
@@ -1,7 +1,50 @@
 import matplotlib.pyplot as plt
 from Map import Map
 
+class State:
+    def __init__(self, position, g):
+        # init unopened state
+        self.checked = False
+
+        # current coordinates
+        self.position = position
+
+        # heuristic values
+        self.g = g
+        self.h = 0
+        self.f = 0
+
+    def calculateF(self, goal):
+        self.calculateH(goal)
+        self.f = self.h + self.g
+        return self.f
+
+    def calculateH(self, goal):
+        self.h = abs(self.position[0] - goal[0]) + abs(self.position[1] - goal[1])
+        return self.h
+
+    def atDestination(self, goal):
+        if self.position != goal:
+            return False
+        return True
+
+
+def AStarSearch(Map):
+    # starting position state
+    positionState = State(Map.start, 0)
+
+    # list of opened and closed points
+    openList = []
+    closedList = []
+
+    # add start to open list
+    
+    while not state.atDestination:
+        pass
+    
+
+
 if __name__ == '__main__':
-    Map1 = Map(30, 20, (0,0))
-    plt.imshow(Map1.map)
+    Map1 = Map(30, 20, (18, 1), (1, 28))
+    plt.imshow(Map1._map)
     plt.show()
